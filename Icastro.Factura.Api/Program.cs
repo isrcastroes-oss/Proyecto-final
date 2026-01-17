d384285e9432529c297d11fdddb43e921781fcf8
Israel Castro <isrcastro.es@tecnologicoargos.edu.ec>
2026-01-17 14:31:10 -0500

    updates

---
+++ Icastro.Factura.Api/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Icastro.Factura.Infrastructure.Data;
+using Icastro.Factura.Domain.Interfaces;
+using Icastro.Factura.Infrastructure.Repositories;
+using Icastro.Factura.Application.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+);
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
